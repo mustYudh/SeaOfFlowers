3009a7565b766bd6bf2230a2b2124a2ab6779474
mks <keshuang.ma@easwift.cn>
2019-07-03 16:36:58 +0800

    commit ui添加

--- app/src/main/java/com/hzrcht/seaofflowers/module/mine/activity/adapter/MarginDecoration.java
+++ app/src/main/java/com/hzrcht/seaofflowers/module/mine/activity/adapter/MarginDecoration.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Rect;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
 import com.yu.common.utils.DensityUtil;
 
 /**
@@ -11,20 +12,21 @@ import com.yu.common.utils.DensityUtil;
  * @date 2019-06-27
  */
 public class MarginDecoration extends RecyclerView.ItemDecoration {
-  private int margin;
-  private int count;
+    private int margin;
+    private int count;
 
-  public MarginDecoration(Context context, int count,int margin) {
-    this.margin = DensityUtil.dip2px(context, margin);
-    this.count = count;
-  }
-
-  @Override public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
-      RecyclerView.State state) {
-    if (parent.getChildLayoutPosition(view) % count == 0) {
-      outRect.set(margin, 0, margin, 0);
-    } else {
-      outRect.set(0, 0, margin, 0);
+    public MarginDecoration(Context context, int count, int margin) {
+        this.margin = DensityUtil.dip2px(context, margin);
+        this.count = count;
     }
-  }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
+                               RecyclerView.State state) {
+        if (parent.getChildLayoutPosition(view) % count == 0) {
+            outRect.set(margin, 0, margin, 0);
+        } else {
+            outRect.set(0, 0, margin, 0);
+        }
+    }
 }
