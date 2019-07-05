dae6646b4115193dc7e84a6276d4d4503fc2def4
yudenghao <denghao1019@163.com>
2019-07-05 10:43:27 +0800

    add:修改上传图片

--- app/src/main/java/com/hzrcht/seaofflowers/module/mine/activity/presenter/MineSetUpAlbumPresenter.java
+++ app/src/main/java/com/hzrcht/seaofflowers/module/mine/activity/presenter/MineSetUpAlbumPresenter.java
@@ -1,46 +1,41 @@
 package com.hzrcht.seaofflowers.module.mine.activity.presenter;
 
 import android.annotation.SuppressLint;
-
+import android.util.Log;
 import com.hzrcht.seaofflowers.http.subscriber.TipRequestSubscriber;
 import com.hzrcht.seaofflowers.module.mine.activity.bean.UploadImgBean;
 import com.xuexiang.xhttp2.XHttp;
 import com.xuexiang.xhttp2.exception.ApiException;
+import com.xuexiang.xhttp2.lifecycle.RxLifecycle;
+import com.xuexiang.xhttp2.subsciber.ProgressLoadingSubscriber;
 import com.yu.common.framework.BaseViewPresenter;
 import com.yu.common.toast.ToastUtils;
-
 import java.io.File;
 
-
+@SuppressLint("CheckResult")
 public class MineSetUpAlbumPresenter extends BaseViewPresenter<MineSetUpAlbumViewer> {
 
     public MineSetUpAlbumPresenter(MineSetUpAlbumViewer viewer) {
         super(viewer);
     }
 
-    @SuppressLint("CheckResult")
+
     public void uploadImg(File file) {
-        XHttp.post("http://huahai.hzrcht.com/api/upload/index")
-                .params("uptype", "oss")
-                .params("file", file)
-                .accessToken(true)
-                .execute(UploadImgBean.class)
-                .subscribeWith(new TipRequestSubscriber<UploadImgBean>() {
-                    @Override
-                    protected void onSuccess(UploadImgBean uploadImgBean) {
-                        assert getViewer() != null;
-                        ToastUtils.show("发送成功");
-                    }
+      XHttp.post("/api/upload/index")
+          .params("uptype", "oss")
+          .uploadFile("file", file, (bytesWritten, contentLength, done) -> {
 
-                    @Override
-                    protected void onError(ApiException apiException) {
-                        super.onError(apiException);
-
-                    }
-                });
+          }).execute(Boolean.class)
+          .compose(RxLifecycle.with(getActivity()).bindToLifecycle())
+          .subscribeWith(new ProgressLoadingSubscriber<Boolean>() {
+            @Override
+            public void onSuccess(Boolean aBoolean) {
+              Log.e("======>","上传成功");
+            }
+          });
     }
 
-    @SuppressLint("CheckResult")
+
     public void addImg(String url) {
         XHttp.post("http://huahai.hzrcht.com/api/img/add")
                 .params("url", url)
---
+++ http-lib/src/main/java/com/xuexiang/xhttp2/lifecycle/ActivityLifecycle.java
@@ -0,0 +1,14 @@
+package com.xuexiang.xhttp2.lifecycle;
+
+/**
+ * @author yudneghao
+ * @date 2019-07-05
+ */
+public enum ActivityLifecycle {
+  onCreate,
+  onStart,
+  onResume,
+  onPause,
+  onStop,
+  onDestroy,
+}
