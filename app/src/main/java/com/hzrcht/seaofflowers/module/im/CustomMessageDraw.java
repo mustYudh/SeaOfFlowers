2ee0a7c34f1838c08eaf878c2cf532995c3ee838
mks <keshuang.ma@easwift.cn>
2019-07-13 11:03:20 +0800

    commit im自定义消息

--- app/src/main/java/com/hzrcht/seaofflowers/module/im/CustomMessageDraw.java
+++ app/src/main/java/com/hzrcht/seaofflowers/module/im/CustomMessageDraw.java
@@ -1,13 +1,20 @@
 package com.hzrcht.seaofflowers.module.im;
 
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.hzrcht.seaofflowers.APP;
+import com.hzrcht.seaofflowers.R;
 import com.tencent.imsdk.TIMCustomElem;
 import com.tencent.imsdk.TIMMessage;
 import com.tencent.qcloud.tim.uikit.modules.chat.layout.message.holder.ICustomMessageViewGroup;
 import com.tencent.qcloud.tim.uikit.modules.chat.layout.message.holder.IOnCustomMessageDrawListener;
 import com.tencent.qcloud.tim.uikit.modules.message.MessageInfo;
+import com.yu.common.glide.ImageLoader;
 
 public class CustomMessageDraw implements IOnCustomMessageDrawListener {
     /**
@@ -22,27 +29,25 @@ public class CustomMessageDraw implements IOnCustomMessageDrawListener {
         // 获取到自定义消息的json数据
         TIMCustomElem elem = (TIMCustomElem) info.getTIMMessage().getElement(0);
         TIMMessage timMessage = info.getTIMMessage();
-        Log.e("aaaacccc",timMessage.toString()+"");
-
+        Log.e("aaaacccc", timMessage.toString() + "..." + elem.getData().toString());
 
         // 自定义的json数据，需要解析成bean实例
+        final CustomMessageData customMessageData = new Gson().fromJson(new String(elem.getData()), CustomMessageData.class);
+        // 通过类型来创建不同的自定义消息展示view
+        switch (customMessageData.type) {
+            case CustomMessageData.TYPE_PRESENT:
+                view = LayoutInflater.from(APP.getInstance()).inflate(R.layout.item_chat_present, null, false);
+                // 把自定义消息view添加到TUIKit内部的父容器里
+                parent.addMessageContentView(view);
+                break;
+        }
 
-//        final CustomMessageData customMessageData = new Gson().fromJson(elem.getData().toString(), CustomMessageData.class);
-//        // 通过类型来创建不同的自定义消息展示view
-//        switch (customMessageData.type) {
-//            case CustomMessageData.TYPE_PRESENT:
-//                view = LayoutInflater.from(APP.getInstance()).inflate(R.layout.item_chat_present, null, false);
-//                // 把自定义消息view添加到TUIKit内部的父容器里
-//                parent.addMessageContentView(view);
-//                break;
-//        }
-//
-//        // 自定义消息view的实现，这里仅仅展示文本信息，并且实现超链接跳转
-//        ImageView iv_present = view.findViewById(R.id.iv_present);
-//        TextView tv_name = view.findViewById(R.id.tv_name);
-//        TextView tv_price = view.findViewById(R.id.tv_price);
-//        tv_name.setText(customMessageData.title);
-//        tv_price.setText(customMessageData.price);
-//        ImageLoader.getInstance().displayImage(iv_present, customMessageData.url);
+        // 自定义消息view的实现，这里仅仅展示文本信息，并且实现超链接跳转
+        ImageView iv_present = view.findViewById(R.id.iv_present);
+        TextView tv_name = view.findViewById(R.id.tv_name);
+        TextView tv_price = view.findViewById(R.id.tv_price);
+        tv_name.setText(customMessageData.title);
+        tv_price.setText(customMessageData.price);
+        ImageLoader.getInstance().displayImage(iv_present, customMessageData.url);
     }
 }
