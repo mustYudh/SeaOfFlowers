5b2d518e0a8e5a56a89fa061b4531380e56d6316
yudenghao <denghao.yu@yuntu-inc.com>
2019-07-25 00:04:35 +0800

    add：修复声音

--- app/src/main/java/com/hzrcht/seaofflowers/module/home/HomeVideoWaitActivity.java
+++ app/src/main/java/com/hzrcht/seaofflowers/module/home/HomeVideoWaitActivity.java
@@ -115,7 +115,7 @@ public class HomeVideoWaitActivity extends BaseActivity implements HomeVideoWait
                 finish();
             });
         }
-        registerVolumeChangeReceiver();
+        //registerVolumeChangeReceiver();
     }
 
     private void registerVolumeChangeReceiver() {
